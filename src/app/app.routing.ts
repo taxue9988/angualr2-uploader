9ba962dbf35bf70afa8ba80d98088aefbf32a19a
taxue9988 <taxuewuhen3@126.com>
2016-12-07 16:41:10 +0800

    Update app.routing.ts
    m

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const AppRoutes:Routes = [
     { path: 'test', loadChildren: 'app/modules/test/test.module#TestModule' },
-    { path: '', redirectTo: '/sign', pathMatch: 'full', },
+    { path: '', redirectTo: '/test', pathMatch: 'full', },
 ];
 
 export const AppRouting = RouterModule.forRoot(AppRoutes);
